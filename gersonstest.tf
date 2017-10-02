4ba869e60262dd24cbdcf1da16cbe13c55cefdc4
ruediger maass <ruediger.maass@de.ibm.com>
2017-10-02 17:50:02 +0200

    initial

---
+++ gersonstest.tf
@@ -0,0 +1,29 @@
+###############################################################################
+### IBM Cloud Provider
+###############################################################################
+
+provider "ibm" {
+    softlayer_username = "${var.softlayer_username}"
+    softlayer_api_key  = "${var.softlayer_api_key}"
+}
+
+resource "ibm_storage_file" "nfs_manager" {
+    count      = 1
+    capacity   = 20
+    datacenter = "${var.datacenter}"
+    iops       = 10
+    type       = "Endurance"
+}
+
+###############################################################################
+### Variables
+###############################################################################
+
+# Softlayer credentials
+variable softlayer_username {}
+variable softlayer_api_key {}
+
+# Softlayer datacenter to deploy the manager
+variable datacenter {
+    default = "dal10"
+}

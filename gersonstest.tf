261944a3ac5fc94e6acc13cbf3a87b49fee0d23f
ruediger maass <ruediger.maass@de.ibm.com>
2017-10-10 10:54:50 +0200

    Update gersonstest.tf

--- gersonstest.tf
+++ gersonstest.tf
@@ -2,7 +2,7 @@
 ### IBM Cloud Provider
 ###############################################################################
 
-provider "ibm" {
+provider "ibm5" {
     softlayer_username = "${var.softlayer_username}"
     softlayer_api_key  = "${var.softlayer_api_key}"
 }
